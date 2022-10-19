b0bbdf27ca07b25dbf77af4a6ce4a59b41bc2e73
YAKUT AHMEDIN <yakutmuder9@gmail.com>
2022-10-18 22:44:11 -0400

    all

--- client/src/screen/pages/pages.js
+++ client/src/screen/pages/pages.js
@@ -1,17 +1,7 @@
 import React from 'react'
 
 const Pages = () => {
-   fetch('https://graph.facebook.com/v15.0/100047168145051')
-   .then(res => res.json)
-   .then(data => 
-    console.log(data))
-  
-//     function (response) {
-//       if (response && !response.error) {
-//         /* handle the result */
-//       }
-//     }
-// );
+
 
   return (
     <div>
