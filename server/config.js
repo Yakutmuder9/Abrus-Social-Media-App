78ed8402e7fb382d86fed622ce437e399f939d2b
YAKUT AHMEDIN <yakutmuder9@gmail.com>
2022-10-21 05:01:36 -0400

    removed_env

--- server/config.js
+++ server/config.js
@@ -2,5 +2,3 @@ module.exports = {
     'secretKey': process.env.SECRET_KEY,
     'mongoUrl': process.env.DATABASE_CONNECTION
 }
-// 'mongoUrl': 'mongodb://localhost:27017/wina'
-    // 'mongoUrl': 'mongodb+srv://furniture:1234@cluster0.0izmyse.mongodb.net/furniture?retryWrites=true&w=majority'
