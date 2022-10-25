0ff69975e58c78b73046f51dc7d1386de18216bf
YAKUT AHMEDIN <yakutmuder9@gmail.com>
2022-10-25 05:27:38 -0400

    test

--- server/app.js
+++ server/app.js
@@ -39,6 +39,7 @@ app.use(cors(corsOptions));
 
 
 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(cookieParser());
@@ -57,11 +58,12 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get('env') === 'production' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
