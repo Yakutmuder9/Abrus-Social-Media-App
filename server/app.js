5e09dedebf259b3389dd9886811e427d5b60220c
YAKUT AHMEDIN <yakutmuder9@gmail.com>
2022-10-25 23:18:52 -0400

    dot

--- server/app.js
+++ server/app.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config({path: './config.env'});
 var createError = require('http-errors');
 var express = require('express');
 var cors = require('cors')
