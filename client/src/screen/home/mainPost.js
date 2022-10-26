ca0a3985e72bab71b697df7f51e0c0b9b83390ad
YAKUT AHMEDIN <yakutmuder9@gmail.com>
2022-10-25 21:43:50 -0400

    dev

--- client/src/screen/home/mainPost.js
+++ client/src/screen/home/mainPost.js
@@ -48,7 +48,7 @@ const MainPost = (prop) => {
     dispatch(fetchAllUsersPosts());
     async function getUserData() {
       const data = await getUser();
-      console.log(data)
+      
       setProfile(data);
       setLoading(false);
       dispatch(SET_USER(data));
